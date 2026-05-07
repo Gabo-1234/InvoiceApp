19eb201a0513f895f00077d63e477d234e6dc72c
GABO <gumbaridzegabrieli@gmail.com>
2026-05-07 23:08:21 +0400

    visual side

--- src/Components/Functions/FormFunctions.jsx
+++ src/Components/Functions/FormFunctions.jsx
@@ -2,4 +2,15 @@ const NewItemMake = () => {
   console.log('New Item Added');
 };
 
-export { NewItemMake };
+export { NewItemMake, showOptions };
+
+const showOptions = () => {
+  // const defaultOption = document.querySelector('.defaultOption');
+  // const hiddenOptions = document.querySelectorAll('.hidden');
+  // defaultOption.addEventListener('click', () => {
+  //   hiddenOptions.forEach(option => {
+  //     option.classList.remove('hidden');
+  //   });
+// })};
+console.log('Show Options Function Called');
+}
--- src/Components/Navigations/EditBar.jsx
+++ src/Components/Navigations/EditBar.jsx
@@ -1,4 +1,4 @@
- import {
+import {
   isRequired,
   isEmail,
   isPositiveNumber,
@@ -6,94 +6,151 @@
 } from "../Functions/FormChecks";
 import "../Css/EditBar.css";
 import { NewItemMake } from "../Functions/FormFunctions";
+import { showOptions } from "../Functions/FormFunctions";
 
 const EditBar = () => {
   return (
     <>
       <form className="EditBar">
-        <h2>Edit #XM9141</h2>
+        <h2>
+          Edit <span>#</span>XM9141
+        </h2>
         <div className="Bill-info">
-          <label htmlFor="Street-adress">Street Address</label>
-          <input type="text" id="Street-adress" onSubmit={isRequired} />
+          <h4 className="h4">Bill From</h4>
+          <div className="Street-AD">
+            <label htmlFor="Street-adress">Street Address</label>
+            <input type="text" id="Street-adress" onSubmit={isRequired} />
+          </div>
           <div className="Three-row">
-            <label htmlFor="City" className="Label-Des1">
-              City
-            </label>
-            <input type="text" id="City" onSubmit={isRequired} />
-            <label htmlFor="Postal-Code" className="Label-Des1">
-              Postal Code
-            </label>
-            <input type="text" id="Postal-Code" onSubmit={isRequired} />
-            <label htmlFor="Country" className="Label-Des1">
-              Country
-            </label>
-            <input type="text" id="Country" onSubmit={isRequired} />
+            <div>
+              <label htmlFor="City" className="Label-Des1">
+                City
+              </label>
+              <input type="text" id="City" onSubmit={isRequired} />
+            </div>
+            <div>
+              <label htmlFor="Postal-Code" className="Label-Des1">
+                Postal Code
+              </label>
+              <input type="text" id="Postal-Code" onSubmit={isRequired} />
+            </div>
+            <div>
+              <label htmlFor="Country" className="Label-Des1">
+                Country
+              </label>
+              <input type="text" id="Country" onSubmit={isRequired} />
+            </div>
           </div>
         </div>
         <div className="Bill-to">
-          <label htmlFor="Client-Name">Clients Name</label>
-          <input type="text" id="Client-Name" onSubmit={isRequired} />
-          <label htmlFor="Client-Email">Clients Email</label>
-          <input
-            type="email"
-            id="Client-Email"
-            placeholder="e.g email@example.com"
-            onSubmit={isRequired}
-          />
-          <label htmlFor="Bill-to">Bill To</label>
-          <input type="text" id="Bill-to" onSubmit={isRequired} />
-          <div className="row">
-            <label htmlFor="City" className="Label-Des1">
-              City
-            </label>
-            <input type="text" id="City" onSubmit={isRequired} />
-            <label htmlFor="Postal-Code" className="Label-Des1">
-              Postal Code
-            </label>
-            <input type="text" id="Postal-Code" onSubmit={isRequired} />
-            <label htmlFor="Country" className="Label-Des1">
-              Country
-            </label>
-            <input type="text" id="Country" onSubmit={isRequired} />
+          <h4 className="h4">Bill To</h4>
+          <div>
+            <label htmlFor="Client-Name">Clients Name</label>
+            <input type="text" id="Client-Name" onSubmit={isRequired} />
           </div>
-        </div>
-        <div className="Date&Terms">
-          <div className="row">
-            <label htmlFor="Invoice-Date">Invoice Date</label>
+          <div>
+            <label htmlFor="Client-Email">Clients Email</label>
             <input
-              type="date"
-              id="Invoice-Date"
-              min="2000-01-01"
-              max={MaxDate}
+              type="email"
+              id="Client-Email"
+              placeholder="e.g email@example.com"
               onSubmit={isRequired}
             />
-            <label htmlFor="Payment-Terms">Payment Terms</label>
-            <select
-              name="Payment-Terms"
-              id="Payment-Terms"
-              defaultValue=""
-              onSubmit={isRequired}
-            >
-              <option value="" disabled>
-                Select Payment Terms
-              </option>
-              <option value="Net 1">Net 1 Days</option>
-              <option value="Net 7">Net 7 Days</option>
-              <option value="Net 14">Net 14 Days</option>
-              <option value="Net 30">Net 30 Days</option>
-            </select>
           </div>
-          <label htmlFor="Project-Description">Project Description</label>
-          <input
-            type="text"
-            id="Project-Description"
-            placeholder="e.g Graphic Design Service"
-            onSubmit={isRequired}
-          />
+          <div>
+            <label htmlFor="Street-adress">Street Address</label>
+            <input type="text" id="Street-adress" onSubmit={isRequired} />
+          </div>
+          <div className="Three-row">
+            <div>
+              <label htmlFor="City" className="Label-Des1">
+                City
+              </label>
+              <input type="text" id="City" onSubmit={isRequired} />
+            </div>
+            <div>
+              <label htmlFor="Postal-Code" className="Label-Des1">
+                Postal Code
+              </label>
+              <input type="text" id="Postal-Code" onSubmit={isRequired} />
+            </div>
+            <div>
+              <label htmlFor="Country" className="Label-Des1">
+                Country
+              </label>
+              <input type="text" id="Country" onSubmit={isRequired} />
+            </div>
+          </div>
+        </div>
+        <div className="DateNTerms">
+          <div className="Three-row" style={{ display: "flex" }}>
+            <div>
+              <label htmlFor="Invoice-Date">Invoice Date</label>
+              <input
+                type="date"
+                id="Invoice-Date"
+                min="2000-01-01"
+                max={MaxDate}
+                onSubmit={isRequired}
+              />
+            </div>
+            <div>
+              <label htmlFor="Payment-Terms">Payment Terms</label>
+              <li
+                name="Payment-Terms"
+                id="Payment-Terms"
+                defaultValue=""
+                onSubmit={isRequired}
+              >
+                <div className="defaultOptionDiv">
+                  <ul
+                    className="defaultOption"
+                    value=""
+                    disabled
+                    onClick={showOptions}
+                  >
+                    Net 30 Days
+                  </ul>
+                  <svg
+                    width="10"
+                    height="7"
+                    viewBox="0 0 10 7"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="Open"
+                  >
+                    <path
+                      d="M0.707092 0.707153L4.93499 4.93505L9.1629 0.707153"
+                      stroke="#7C5DFA"
+                      stroke-width="2"
+                    />
+                  </svg>
+                </div>
+                <div className="Option-bar">
+                  <ul value="Net 1">Net 1 Days</ul>
+                  <hr />
+                  <ul value="Net 7">Net 7 Days</ul>
+                  <hr />
+                  <ul value="Net 14">Net 14 Days</ul>
+                  <hr />
+                  <ul value="Net 30">Net 30 Days</ul>
+                </div>
+              </li>
+            </div>
+          </div>
+          <div>
+            <label htmlFor="Project-Description">Project Description</label>
+            <input
+              type="text"
+              id="Project-Description"
+              placeholder="e.g Graphic Design Service"
+              onSubmit={isRequired}
+            />
+          </div>
         </div>
         <div className="Item-list">
           <button className="Add-new" onClick={NewItemMake}>
-            +Add New Item
+            + Add New Item
           </button>
         </div>
         <button>Cancel</button>
